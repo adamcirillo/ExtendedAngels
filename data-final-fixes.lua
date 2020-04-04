7006c568c08cb9f171e897efecb4d8c84ec3ff23
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-04-05 09:12:54 +1000

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -24,13 +24,16 @@ for _,recipe in pairs(recipeList) do
   end
 end
 
---angels component updates
 if mods["angelsindustries"] then
   if settings.startup["angels-enable-components"].value or settings.startup["angels-enable-tech"].value then
     require("prototypes.recipes.angels-components")
     local OV = angelsmods.functions.OV
     rep_con_mats()
     OV.execute()
+    for n,_ in pairs(perviousbuilding) do 
+      table.remove(data.raw["recipe"][_]["normal"].ingredients,1); 
+      table.remove(data.raw["recipe"][_]["expensive"].ingredients,1); 
+    end
     if settings.startup["angels-return-ingredients"].value then
       add_minable_results()
       OV.execute()
