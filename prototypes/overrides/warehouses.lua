ebcf0e7e290ec61c6adebee202bd300c0de5cc57
Adam Cirillo <adamncirillo@gmail.com>
2026-01-11 10:41:56 +1100

    Icon scaling fix

--- prototypes/overrides/warehouses.lua
+++ prototypes/overrides/warehouses.lua
@@ -19,25 +19,25 @@ end
 -- Icon scaling
 if angelsmods.addons.storage.icon_scaling then
     if (mods["angelsaddons-storage"] and angelsmods.addons.storage.warehouses) then
-        data.raw["container"]["angels-warehouse-mk2"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk2"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-active-provider-mk2"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-storage-mk2"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-requester-mk2"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-buffer-mk2"].icons {{scale_info_icons = true}}
+        data.raw["container"]["angels-warehouse-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-active-provider-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-storage-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-requester-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-buffer-mk2"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
 
-        data.raw["container"]["angels-warehouse-mk3"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk3"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-active-provider-mk3"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-storage-mk3"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-requester-mk3"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-buffer-mk3"].icons {{scale_info_icons = true}}
+        data.raw["container"]["angels-warehouse-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-active-provider-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-storage-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-requester-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-buffer-mk3"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
 
-        data.raw["container"]["angels-warehouse-mk4"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk4"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-active-provider-mk4"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-storage-mk4"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-requester-mk4"].icons {{scale_info_icons = true}}
-        data.raw["logistic-container"]["angels-warehouse-buffer-mk4"].icons {{scale_info_icons = true}}
+        data.raw["container"]["angels-warehouse-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-passive-provider-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-active-provider-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-storage-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-requester-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
+        data.raw["logistic-container"]["angels-warehouse-buffer-mk4"].icon_draw_specification = {scale = 3.5, scale_for_many = 3.5}
     end
 end
