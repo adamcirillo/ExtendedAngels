c4c669224ed1b4de6e58aed8bea8736bf8586f37
Adam Cirillo <adamncirillo@gmail.com>
2026-01-10 20:38:44 +1100

    fixed fluid boxes on induction smelter again

--- prototypes/overrides/smelting.lua
+++ prototypes/overrides/smelting.lua
@@ -1,16 +1,16 @@
 -- Add input fluid_box to Induction Furnace
 for n = 1, 4 do
-    local name = (n > 1) and "induction-furnace-" .. n or "induction-furnace"
+    local name = (n > 1) and "angels-induction-furnace-" .. n or "angels-induction-furnace"
     local entity = data.raw["assembling-machine"][name]
 
     if not entity then goto continue end
 
     table.insert(entity.fluid_boxes, {
         production_type = "input",
-        pipe_covers = angelsmods.pipecoverspictures(),
+        pipe_covers = pipecoverspictures(),
         volume = 1000,
         base_level = -1,
-        pipe_connections = { { position = { -2, 3 } } },
+        pipe_connections = { { flow_direction = "input", position = { -2, 2 }, direction = defines.direction.south } },
     })
 
     ::continue::
