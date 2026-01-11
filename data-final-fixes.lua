f459c772b3a180fd644c8f7e79ce77d45733be1c
Kira <kirazy@live.com>
2026-01-11 13:32:36 -0800

    refactor: consolidate industry component fixes, simplify

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,2 @@
 require("prototypes.recipes.smelting-final-fixes")
-
-require("prototypes.recipes.angels-components")
-require("prototypes.recipes.industries-final-fixes")
+require("prototypes.recipes.industries-components-final-fixes")
