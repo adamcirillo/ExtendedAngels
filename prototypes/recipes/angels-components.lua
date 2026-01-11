69b41c8306131e082da209cedf6e26a863d0c176
Kira <kirazy@live.com>
2026-01-11 13:20:11 -0800

    refactor: regroup/simplify component update methods

--- prototypes/recipes/angels-components.lua
+++ prototypes/recipes/angels-components.lua
@@ -2,10 +2,50 @@ if not (mods["angelsindustries"] and angelsmods.industries.components) then
   return
 end
 
--- Final fixes of angels to squeeze in a new construction tier material
-local OV = angelsmods.functions.OV
+-- Revise the construction block ingredients.
+local block_ingredients = {
+  ["block-construction-0"] = { new = "stone", old = "stone" },
+  ["block-construction-1"] = { new = "stone-brick", old = "stone" },
+  ["block-construction-2"] = { new = "angels-clay-brick", old = "stone-brick" },
+  ["block-construction-3"] = { new = "concrete-brick", old = "angels-clay-brick" },
+  ["block-construction-4"] = { new = "angels-reinforced-concrete-brick", old = "concrete-brick" },
+  ["block-construction-5"] = { new = "angels-titanium-concrete-brick", old = "angels-reinforced-concrete-brick" },
+}
 
--- Set local table for use in multiple functions
+local function change_construction_block_ingredients()
+  for block, list in pairs(block_ingredients) do
+    local ingredients = data.raw.recipe[block].ingredients
+
+    if ingredients then
+      for n, _ in pairs(ingredients) do
+        if ingredients[n].name == list.old then
+          ingredients[n].name = list.new
+        end
+      end
+    end
+  end
+end
+
+-- Shift the construction block ingredients down one tier for sets of recipe ingredients.
+local remapped_ingredients = {
+  ["block-construction-1"] = "block-construction-0",
+  ["block-construction-2"] = "block-construction-1",
+  ["block-construction-3"] = "block-construction-2",
+  ["block-construction-4"] = "block-construction-3",
+  ["block-construction-5"] = "block-construction-4",
+  ["angels-titanium-concrete-brick"] = "block-construction-5",
+}
+
+local function shift_block_ingredients(ingredients)
+  for n, _ in pairs(ingredients) do
+    -- If the ingredient has a defined remap, apply it.
+    if remapped_ingredients[ingredients[n].name] then
+      ingredients[n].name = remapped_ingredients[ingredients[n].name]
+    end
+  end
+end
+
+-- ADD BUILDING BLOCKS TO BUILDINGS
 local building_prototypes = {
   "assembling-machine",
   "mining-drill",
@@ -26,91 +66,6 @@ local building_prototypes = {
   "gate",
 }
 
-local block_ingredients = {
-  ["block-construction-0"] = { new = "stone", old = "stone" },
-  ["block-construction-1"] = { new = "stone-brick", old = "stone" },
-  ["block-construction-2"] = { new = "angels-clay-brick", old = "stone-brick" },
-  ["block-construction-3"] = { new = "concrete-brick", old = "angels-clay-brick" },
-  ["block-construction-4"] = { new = "angels-reinforced-concrete-brick", old = "concrete-brick" },
-  ["block-construction-5"] = { new = "angels-titanium-concrete-brick", old = "angels-reinforced-concrete-brick" },
-}
-
--- Add a 0th tier construction block
-data:extend({
-  {
-    type = "item",
-    name = "block-construction-0", -- required at start
-    icon = "__angelsindustriesgraphics__/graphics/icons/block-construction-1.png",
-    icon_size = 32,
-    subgroup = "blocks-frames",
-    order = "a",
-    stack_size = 200,
-  },
-
-  {
-    type = "recipe",
-    name = "block-construction-0",
-    enabled = true,
-    category = "crafting",
-    energy_required = 5,
-    ingredients = {
-      { type = "item", name = "construction-frame-1", amount = 1 },
-      { type = "item", name = "stone", amount = 3 },
-    },
-    results = {
-      { type = "item", name = "block-construction-0", amount = 1 },
-    },
-  },
-})
-
--- Replace ingredients
-local function change_construction_block_ingredients()
-  for block, list in pairs(block_ingredients) do
-    local ingredients = data.raw.recipe[block].ingredients
-
-    if ingredients then
-      for n, _ in pairs(ingredients) do
-        if ingredients[n].name == list.old then
-          ingredients[n].name = list.new
-        end
-      end
-    end
-
-    -- For Logging
-    -- log(serpent.block(ingredients))
-  end
-end
-
--- Shifts the construction block ingredients down one tier
-local shift_block_ingredients = function(ingredients)
-  for n, _ in pairs(ingredients) do
-    if ingredients[n].name == "block-construction-1" then
-      ingredients[n].name = "block-construction-0"
-    end
-
-    if ingredients[n].name == "block-construction-2" then
-      ingredients[n].name = "block-construction-1"
-    end
-
-    if ingredients[n].name == "block-construction-3" then
-      ingredients[n].name = "block-construction-2"
-    end
-
-    if ingredients[n].name == "block-construction-4" then
-      ingredients[n].name = "block-construction-3"
-    end
-
-    if ingredients[n].name == "block-construction-5" then
-      ingredients[n].name = "block-construction-4"
-    end
-
-    if ingredients[n].name == "angels-titanium-concrete-brick" then
-      ingredients[n].name = "block-construction-5"
-    end
-  end
-end
-
--- ADD BUILDING BLOCKS TO BUILDINGS
 function extangels.replace_construction_materials()
   for _, prototype in pairs(building_prototypes) do
     extangels.shuffle_construction_materials(prototype)
