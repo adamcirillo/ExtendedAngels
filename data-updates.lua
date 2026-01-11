79b39d278815c71f78a31a8188e3fd2707061476
Kira <kirazy@live.com>
2026-01-11 13:06:53 -0800

    refactor: move smelting updates out of data-updates file

--- data-updates.lua
+++ data-updates.lua
@@ -2,6 +2,7 @@
 require("prototypes.recipes.bioprocessing")
 require("prototypes.recipes.petrochem")
 require("prototypes.recipes.refining")
+require("prototypes.recipes.smelting-updates")
 require("prototypes.recipes.warehouses")
 
 -- Overrides
@@ -12,9 +13,3 @@ require("prototypes.overrides.recipes")
 require("prototypes.overrides.smelting")
 require("prototypes.overrides.technology")
 require("prototypes.overrides.warehouses")
-
--- Add numeral to angels-powder-tungsten as we add a second tier of recipe
-data.raw.recipe["angels-powder-tungsten"].icons = extangels.numeral_tier({
-  icon = "__angelssmeltinggraphics__/graphics/icons/powder-tungsten.png",
-  icon_size = 64,
-}, 1, angelsmods.smelting.number_tint)
